b2899d5b7ff9435723e01ae4b0088eb0b627133e
machida <machida@kmc.gr.jp>
2020-12-20 18:38:20 +0900

    add passing test

---
+++ test/statement-test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const statement = require('../statement');
+const invoice = require('../invoices');
+const plays = require('../plays');
+
+describe("Statement", () => {
+  it('generates a statement', () => {
+    const expected = `Statement for BigCo\n  Hamlet: $650.00 (55 seats)\n  As You Like It: $580.00 (35 seats)\n  Othello: $500.00 (40 seats)\nAmount owed is $1,730.00\nYou earned 47 credits\n`;
+    const result = statement(invoice[0], plays);
+
+    assert.strictEqual(expected, result);
+  });
+});
